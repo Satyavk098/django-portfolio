0461d019bd691f54947d2a2144d91cde37069475
Satyavk098 <satyamv927@gmail.com>
2026-02-20 01:06:40 +0530

    Fix database config and add dj-database-url

--- myproject/settings.py
+++ myproject/settings.py
@@ -89,15 +89,12 @@ import os
 import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
+    'default': dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=True
     )
 }
-
-
-
 
 
 
