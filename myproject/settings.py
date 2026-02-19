bf084f0ade97c0e17fb8076da9cd3bcabc64cf5f
Satyavk098 <satyamv927@gmail.com>
2026-02-20 00:45:35 +0530

    Fix database config for Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,14 +85,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-import dj_database_url
 import os
+import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
+
+
+
 
 
 
