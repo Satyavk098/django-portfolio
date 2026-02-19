9254766f4326e9982bb2abefe1be9dcc03d411a8
Satyavk098 <satyamv927@gmail.com>
2026-02-20 02:04:27 +0530

    Remove all model access from settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -163,14 +163,4 @@ CSRF_TRUSTED_ORIGINS = [
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        "admin",
-        "admin@example.com",
-        "admin123"
-    )
 
